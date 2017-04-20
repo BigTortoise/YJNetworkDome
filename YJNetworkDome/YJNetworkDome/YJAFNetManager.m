60691fe3aa12428cb6b7390a63d877cb5a8684c9
RYJ <devren@192.168.10.250>
2017-04-20 09:25:45 +0800

    完善网络请求封装

--- YJNetworkDome/YJNetworkDome/YJAFNetManager.m
+++ YJNetworkDome/YJNetworkDome/YJAFNetManager.m
@@ -11,7 +11,7 @@
 
 @implementation YJAFNetManager
 
-+ (void)GET_Path:(NSString *)path completed:(YJSuccessBlock)successBlock failed:(YJFailedBlock)failed {
++ (void)GET_Path:(NSString *)path completed:(YJSuccessBlock)success failed:(YJFailedBlock)failed {
     // 路径中的中文、特殊符号&％和空格都必须进行转译
     path = [path stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
     
@@ -27,7 +27,7 @@
     // GET请求
     [manager GET:path parameters:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
         
-        successBlock([NSJSONSerialization dataWithJSONObject:responseObject options:NSJSONWritingPrettyPrinted error:nil],responseObject);
+        success([NSJSONSerialization dataWithJSONObject:responseObject options:NSJSONWritingPrettyPrinted error:nil],responseObject);
     } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
         
         failed(error);
@@ -35,15 +35,44 @@
 }
 
 + (void)GET_Path:(NSString *)path  params:(NSDictionary *)params  completed:(YJSuccessBlock )success failed:(YJFailedBlock )failed {
-    
+    //1.获得请求管理者
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    [manager GET:path parameters:params success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        
+        success([NSJSONSerialization dataWithJSONObject:responseObject options:NSJSONWritingPrettyPrinted error:nil],responseObject);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        
+        failed(error);
+    }];
 }
 
 + (void)POST_Path:(NSString *)path completed:(YJSuccessBlock )success failed:(YJFailedBlock )failed {
+    path = [path stringByAddingPercentEncodingWithAllowedCharacters:[NSCharacterSet URLQueryAllowedCharacterSet]];
     
+    
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    // 添加 text/html 类型到可接收内容类型中
+    manager.responseSerializer.acceptableContentTypes= [NSSet setWithObjects:@"application/json", @"text/json", @"text/javascript", @"text/html", nil];
+    [manager POST:path parameters:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        
+        success([NSJSONSerialization dataWithJSONObject:responseObject options:NSJSONWritingPrettyPrinted error:nil],responseObject);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        
+        failed(error);
+    }];
 }
 
 + (void)POST_Path:(NSString *)path params:(id)paramsDic completed:(YJSuccessBlock )success failed:(YJFailedBlock )failed {
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
     
+    manager.requestSerializer=[AFJSONRequestSerializer serializer];
+    [manager POST:path parameters:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        
+        success([NSJSONSerialization dataWithJSONObject:responseObject options:NSJSONWritingPrettyPrinted error:nil],responseObject);
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        
+        failed(error);
+    }];
 }
 
 +(void)downloadFile:(NSString *)UrlAddress  completed:(YJSuccessBlock)successBlock failed:(YJFailedBlock)failed  progress:(YJSpeedBlock)progressBlock {
